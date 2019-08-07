3547886e0dc123373b82541f6e6ef3fe7f597245
John Graham <john.a.graham@gmail.com>
2019-08-07 17:17:34 -0500

    initial commit

---
+++ foo.py
@@ -0,0 +1,11 @@
+def foo(a, b):
+    print("hello world")
+
+    x = 3
+
+    print ("goodbye")
+    print ("world", b)
+
+    x = 5
+
+foo(1, 2)
---
+++ test_foo.py
@@ -0,0 +1,6 @@
+import foo
+
+def test_foo():
+    x = 1
+    y = 2
+    assert x == y
